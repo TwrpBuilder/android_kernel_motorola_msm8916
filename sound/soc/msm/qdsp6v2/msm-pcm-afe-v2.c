29ee706f58e460f4ea6eae1105a1f9425fee4137
Xiaojun Sang <xsang@codeaurora.org>
2017-06-13 11:20:15 +0530

    ASoC: msm: qdsp6v2: set pointer to NULL after free
    Pointer after kfree is not sanitized.
    Set pointer to NULL.
    
    Mot-CRs-fixed: (CR)
    CVE-fixed: CVE-2017-8246
    Bug-Id: A-37275839
    CRs-Fixed: 2008031
    Change-Id: Ia59a57fcd142a6ed18d168992b8da4019314afa4
    Signed-off-by: Xiaojun Sang <xsang@codeaurora.org>
    
    Signed-off-by: Neeraj Kumar <neerajk@motorola.com>
    Change-Id: I47487d91d5a43f00a0067Montana47741c1a38baf840
    Reviewed-on: https://gerrit.mot.com/1010813
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-by: Vrushali Prakash Bhosale <wkvq37@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/msm/qdsp6v2/msm-pcm-afe-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-afe-v2.c
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 /* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+=======
+/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+>>>>>>> 8dd68a7f8529... ASoC: msm: qdsp6v2: set pointer to NULL after free
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License version 2 and
@@ -493,6 +497,7 @@ done:
 	mutex_unlock(&prtd->lock);
 	prtd->prepared--;
 	kfree(prtd);
+	runtime->private_data = NULL;
 	return 0;
 }
 static int msm_afe_prepare(struct snd_pcm_substream *substream)
